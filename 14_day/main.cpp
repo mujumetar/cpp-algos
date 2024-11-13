1d67e168484fae282427ea26a2ee9f8154d02872
WebersIO <muzammilmetar82@gmail.com>
2024-11-13 16:37:07 +0530

    chars projects like pelledium algoritham

---
+++ 14_day/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	cout << "input the value: ";
+	cin >> str;
+	
+	string str2;
+	int strlen = str.length();
+	cout << str;
+	
+
+	for(int i = strlen-1;i>=0 ;i--) 
+	{
+		str2 = str2 + str[i];
+	}
+	
+	cout << endl << endl;
+	if(str == str2)
+	{
+		cout << "pelledium";
+	}
+	else
+	{
+		cout << "non pelledium";
+	}
+}
